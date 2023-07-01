ecd085a25bf4d94347af49cc8c5bfe7b22b44e15
reynoldputra <reynoldputra1@gmail.com>
2023-07-02 01:34:36 +0700

    fix: error timeout

--- catscrap.js
+++ catscrap.js
@@ -1,12 +1,13 @@
 import csv from "csvtojson"
 import { getCatUrl, getDataFromHtml, getShopInfoFromHtml, scrapCatList } from "./lib/catscraplib.js"
 import { productScrap } from "./lib/scrap.js"
+import { writeToJson } from "./lib/write.js"
 
 const main = async () => {
   const json = await csv()
     .fromFile('./data/catmapping.csv')
 
-  const concurrencyLimit = 5
+  const concurrencyLimit = 1
 
   for (let i = 0; i < json.length; i += concurrencyLimit) {
     const batchJson = json.slice(i, i + concurrencyLimit);
@@ -24,44 +25,55 @@ const scrapCategories = (catObj) => {
 
     let i = 1;
     while (true) {
-      const allCardsProduct = await getDataFromHtml(catUrl + `?page=${i}`)
-      if(allCardsProduct.length == 0) break
-      i++;
-      const scrapResult = []
-      for (const idx in allCardsProduct) {
-        const product = allCardsProduct[idx]
-        const gaKey = product.ga_key;
-        const routes = gaKey.split("/");
-        let productSlug = routes[routes.length - 1];
-        let shopDomain = routes[routes.length - 2];
-        const pdRes = await productScrap(productSlug, shopDomain);
-        const productInfo = pdRes.basicInfo;
-        let catSlug = `${productInfo.category.detail[0].name}-${productInfo.category.detail[1].name}-${productInfo.category.detail[2].name}`;
+      try {
 
-        let shopUrl = product.url.split("/").slice(0, 4).join("/")
+        const allCardsProduct = await getDataFromHtml(catUrl + `?page=${i}`)
+        if (allCardsProduct.length == 0) break
+        i++;
+        const scrapResult = []
+        for (const idx in allCardsProduct) {
+          const product = allCardsProduct[idx]
+          const gaKey = product.ga_key;
+          const routes = gaKey.split("/");
+          let productSlug = routes[routes.length - 1];
+          let shopDomain = routes[routes.length - 2];
+          const pdRes = await productScrap(productSlug, shopDomain);
+          if (!pdRes) {
+            console.log("Skipped")
+            continue
+          }
 
-        let shopInfoAdd = await getShopInfoFromHtml(shopUrl)
+          const productInfo = pdRes.basicInfo;
+          let catSlug = `${productInfo.category.detail[0].name}-${productInfo.category.detail[1].name}-${productInfo.category.detail[2].name}`;
 
-        const newData = {
-          cat_slug: catSlug,
-          itemid: product.id.toString(),
-          shopid: productInfo.shopID,
-          product_title: product.name,
-          product_link: product.url,
-          brand: null,
-          store_type: shopInfoAdd.isOfficial,
-          store_name: productInfo.shopName,
-          store_link: shopUrl,
-          store_location: shopInfoAdd.location,
-          price: product.price_int,
-          rating: productInfo.stats.rating,
-          historical_sold: parseInt(productInfo.txStats.countSold),
-          review_count: parseInt(productInfo.stats.countReview),
-          view_count: parseInt(productInfo.stats.countView)
-        };
+          let shopUrl = product.url.split("/").slice(0, 4).join("/")
 
-        console.log(newData)
-        scrapResult.push(newData)
+          let shopInfoAdd = await getShopInfoFromHtml(shopUrl)
+
+          const newData = {
+            cat_slug: catSlug,
+            itemid: product.id.toString(),
+            shopid: productInfo.shopID,
+            product_title: product.name,
+            product_link: product.url,
+            brand: null,
+            store_type: shopInfoAdd.isOfficial,
+            store_name: productInfo.shopName,
+            store_link: shopUrl,
+            store_location: shopInfoAdd.location,
+            price: product.price_int,
+            rating: productInfo.stats.rating,
+            historical_sold: parseInt(productInfo.txStats.countSold),
+            review_count: parseInt(productInfo.stats.countReview),
+            view_count: parseInt(productInfo.stats.countView)
+          };
+
+          console.log(newData)
+          scrapResult.push(newData)
+          writeToJson("./export/cat.json", newData)
+        }
+      } catch (err) {
+        console.log("Error : ", err)
       }
     }
     resolve(scrapResult)
--- keywordscrap.js
+++ keywordscrap.js
@@ -1,13 +1,13 @@
 import { keywordScrapAllPage, productScrap } from "./lib/scrap.js";
 import { toInt } from "./lib/utils.js";
-import { writeToJson } from "./lib/write.js";
 import csv from "csvtojson"
+import { writeToJson } from "./lib/write.js";
 
 const main = async () => {
   const json = await csv()
     .fromFile('./data/keywordtoscrap.csv')
   const keywords = json.map((obj) => obj.keyword)
-  const concurrencyLimit = 5
+  const concurrencyLimit = 1
 
   for (let i = 0; i < keywords.length; i += concurrencyLimit) {
     const batchKeywords = keywords.slice(i, i + concurrencyLimit);
@@ -23,41 +23,51 @@ const scrapProductbyKeyword = async (keyword) => {
     const scrapResult = []
 
     for (const idx in products) {
-      const product = products[idx]
-      const gaKey = product.gaKey;
-      const routes = gaKey.split("/");
-      let productSlug = routes[routes.length - 1];
-      let shopDomain = routes[routes.length - 2];
-      const shopId = product.shop.shopId.toString();
-      const pdRes = await productScrap(productSlug, shopDomain);
-      const productInfo = pdRes.basicInfo;
+      try {
+        const product = products[idx]
+        console.log(product)
+        const gaKey = product.gaKey;
+        const routes = gaKey.split("/");
+        let productSlug = routes[routes.length - 1];
+        let shopDomain = routes[routes.length - 2];
+        const shopId = product.shop.shopId.toString();
+        const pdRes = await productScrap(productSlug, shopDomain);
+        if (!pdRes) {
+          console.log("Skipped")
+          continue
+        }
+        const productInfo = pdRes.basicInfo;
 
-      let catSlug = `${productInfo.category.detail[0].name}-${productInfo.category.detail[1].name}-${productInfo.category.detail[2].name}`;
+        let catSlug = `${productInfo.category.detail[0].name}-${productInfo.category.detail[1].name}-${productInfo.category.detail[2].name}`;
 
-      const newData = {
-        keyword,
-        cat_slug: catSlug,
-        itemid: product.id.toString(),
-        shopid: shopId,
-        product_title: product.name,
-        product_link: product.url,
-        brand: null,
-        store_type: product.shop.isOfficial,
-        store_name: product.shop.name,
-        store_link: product.shop.url,
-        store_location: product.shop.city,
-        price: toInt(product.price),
-        rating: productInfo.stats.rating,
-        historical_sold: parseInt(productInfo.txStats.countSold),
-        review_count: parseInt(productInfo.stats.countReview),
-        view_count: parseInt(productInfo.stats.countView)
-      };
+        const newData = {
+          keyword,
+          cat_slug: catSlug,
+          itemid: product.id.toString(),
+          shopid: shopId,
+          product_title: product.name,
+          product_link: product.url,
+          brand: null,
+          store_type: product.shop.isOfficial,
+          store_name: product.shop.name,
+          store_link: product.shop.url,
+          store_location: product.shop.city,
+          price: toInt(product.price),
+          rating: productInfo.stats.rating,
+          historical_sold: parseInt(productInfo.txStats.countSold),
+          review_count: parseInt(productInfo.stats.countReview),
+          view_count: parseInt(productInfo.stats.countView)
+        };
 
-      console.log(newData);
-      scrapResult.push(newData);
+        console.log(newData);
+        writeToJson(`./export/result.json`, newData)
+        scrapResult.push(newData);
+      } catch (err) {
+        console.log("Error : ", err)
+      }
     };
 
-    writeToJson(`./export/result.json`, scrapResult)
+    // writeToJson(`./export/result.json`, scrapResult)
     resolve(scrapResult)
   })
 
