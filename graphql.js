85d14505d0eced0c0588619eeaafbff47b7dece5
reynoldputra <reynoldputra1@gmail.com>
2023-07-01 12:45:39 +0700

    feat: synchronus scrap by keyword

--- graphql.js
+++ graphql.js
@@ -1,89 +1,59 @@
-import axios from "axios";
+import { keywordScrapAllPage, productScrap } from "./lib/scrap.js";
+import { toInt } from "./lib/utils.js";
+import { writeToJson } from "./lib/write.js";
 
+const keywords = [
+  "soflens pinkrabbit",
+  "soflens"
+]
 
-
-const main = () => {
-
+const main = async () => {
+  const finalresult = await scrapProductbyKeyword(keywords[0])
+  writeToJson(`./export/result.json`, finalresult)
 }
 
-const keywordScrap = async (keyword) => {
-  let data = JSON.stringify({
-    "operationName": "SearchProductQueryV4",
-    "query": "query SearchProductQueryV4($params: String!) {\n  ace_search_product_v4(params: $params) {\n    header {\n      totalData\n      totalDataText\n      processTime\n      responseCode\n      errorMessage\n      additionalParams\n      keywordProcess\n      componentId\n      __typename\n    }\n    data {\n      banner {\n        position\n        text\n        imageUrl\n        url\n        componentId\n        trackingOption\n        __typename\n      }\n      backendFilters\n      isQuerySafe\n      ticker {\n        text\n        query\n        typeId\n        componentId\n        trackingOption\n        __typename\n      }\n      redirection {\n        redirectUrl\n        departmentId\n        __typename\n      }\n      related {\n        position\n        trackingOption\n        relatedKeyword\n        otherRelated {\n          keyword\n          url\n          product {\n            id\n            name\n            price\n            imageUrl\n            rating\n            countReview\n            url\n            priceStr\n            wishlist\n            shop {\n              city\n              isOfficial\n              isPowerBadge\n              __typename\n            }\n            ads {\n              adsId: id\n              productClickUrl\n              productWishlistUrl\n              shopClickUrl\n              productViewUrl\n              __typename\n            }\n            badges {\n              title\n              imageUrl\n              show\n              __typename\n            }\n            ratingAverage\n            labelGroups {\n              position\n              type\n              title\n              url\n              __typename\n            }\n            componentId\n            __typename\n          }\n          componentId\n          __typename\n        }\n        __typename\n      }\n      suggestion {\n        currentKeyword\n        suggestion\n        suggestionCount\n        instead\n        insteadCount\n        query\n        text\n        componentId\n        trackingOption\n        __typename\n      }\n      products {\n        id\n        name\n        ads {\n          adsId: id\n          productClickUrl\n          productWishlistUrl\n          productViewUrl\n          __typename\n        }\n        badges {\n          title\n          imageUrl\n          show\n          __typename\n        }\n        category: departmentId\n        categoryBreadcrumb\n        categoryId\n        categoryName\n        countReview\n        customVideoURL\n        discountPercentage\n        gaKey\n        imageUrl\n        labelGroups {\n          position\n          title\n          type\n          url\n          __typename\n        }\n        originalPrice\n        price\n        priceRange\n        rating\n        ratingAverage\n        shop {\n          shopId: id\n          name\n          url\n          city\n          isOfficial\n          isPowerBadge\n          __typename\n        }\n        url\n        wishlist\n        sourceEngine: source_engine\n        __typename\n      }\n      violation {\n        headerText\n        descriptionText\n        imageURL\n        ctaURL\n        ctaApplink\n        buttonText\n        buttonType\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n",
-    "variables": {
-      "params": `device=desktop&navsource=&ob=23&page=1&q=${encodeURI(keyword)}&related=true&rows=200&safe_search=false&scheme=https&shipping=&show_adult=false&source=search&srp_component_id=02.01.00.00&srp_page_id=&srp_page_title=&st=product&start=0&topads_bucket=true&unique_id=197e744d0985e2c43232c573e1f2cd5e&user_addressId=&user_cityId=176&user_districtId=2274&user_id=&user_lat=&user_long=&user_postCode=&user_warehouseId=12210375&variants=&warehouses=`
-    }
-  });
+const scrapProductbyKeyword = async (keyword) => {
+  const products = await keywordScrapAllPage(keyword)
+  const scrapResult = []
 
-  let config = {
-    method: 'post',
-    maxBodyLength: Infinity,
-    url: 'https://gql.tokopedia.com/graphql/SearchProductQueryV4',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: data
+  for (const idx in products) {
+    const product = products[idx]
+    const gaKey = product.gaKey;
+    const routes = gaKey.split("/");
+    let productSlug = routes[routes.length - 1];
+    let shopDomain = routes[routes.length - 2];
+    const shopId = product.shop.shopId.toString();
+    const pdRes = await productScrap(productSlug, shopDomain, shopId);
+    const productInfo = pdRes.basicInfo;
+
+    let catSlug = `${productInfo.category.detail[0].name}-${productInfo.category.detail[1].name}-${productInfo.category.detail[2].name}`;
+
+    const newData = {
+      keyword,
+      cat_slug: catSlug,
+      itemid: product.id.toString(),
+      shopid: shopId,
+      product_title: product.name,
+      product_link: product.url,
+      brand: null,
+      store_type: product.shop.isOfficial,
+      store_name: product.shop.name,
+      store_link: product.shop.url,
+      store_location: product.shop.city,
+      price: toInt(product.price),
+      rating: productInfo.stats.rating,
+      historical_sold: parseInt(productInfo.txStats.countSold),
+      review_count: parseInt(productInfo.stats.countReview),
+      view_count: parseInt(productInfo.stats.countView)
+    };
+
+    console.log(idx);
+    console.log(newData);
+    scrapResult.push(newData);
+
   };
 
-  const res = await axios.request(config)
-  return res.data
+  return scrapResult
 }
 
-const productScrap = async (productKey, shopDomain, shopId) => {
-  let data = JSON.stringify([
-    {
-      "operationName": "PDPGetLayoutQuery",
-      "variables": {
-        "shopDomain": shopDomain,
-        "productKey": productKey,
-        "layoutID": "",
-        "apiVersion": 1,
-        "tokonow": {
-          "shopID": shopId
-        },
-        "userLocation": {
-          "cityID": "176",
-          "addressID": "0",
-          "districtID": "2274",
-          "postalCode": "",
-          "latlon": ""
-        },
-        "extParam": "src%3Dmultiloc%26whid%3D10203116"
-      },
-      "query": "fragment ProductVariant on pdpDataProductVariant {\n  errorCode\n  parentID\n  defaultChild\n  sizeChart\n  totalStockFmt\n  variants {\n    productVariantID\n    variantID\n    name\n    identifier\n    option {\n      picture {\n        urlOriginal: url\n        urlThumbnail: url100\n        __typename\n      }\n      productVariantOptionID\n      variantUnitValueID\n      value\n      hex\n      stock\n      __typename\n    }\n    __typename\n  }\n  children {\n    productID\n    price\n    priceFmt\n    optionID\n    optionName\n    productName\n    productURL\n    picture {\n      urlOriginal: url\n      urlThumbnail: url100\n      __typename\n    }\n    stock {\n      stock\n      isBuyable\n      stockWordingHTML\n      minimumOrder\n      maximumOrder\n      __typename\n    }\n    isCOD\n    isWishlist\n    campaignInfo {\n      campaignID\n      campaignType\n      campaignTypeName\n      campaignIdentifier\n      background\n      discountPercentage\n      originalPrice\n      discountPrice\n      stock\n      stockSoldPercentage\n      startDate\n      endDate\n      endDateUnix\n      appLinks\n      isAppsOnly\n      isActive\n      hideGimmick\n      isCheckImei\n      minOrder\n      __typename\n    }\n    thematicCampaign {\n      additionalInfo\n      background\n      campaignName\n      icon\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment ProductMedia on pdpDataProductMedia {\n  media {\n    type\n    urlOriginal: URLOriginal\n    urlThumbnail: URLThumbnail\n    urlMaxRes: URLMaxRes\n    videoUrl: videoURLAndroid\n    prefix\n    suffix\n    description\n    variantOptionID\n    __typename\n  }\n  videos {\n    source\n    url\n    __typename\n  }\n  __typename\n}\n\nfragment ProductCategoryCarousel on pdpDataCategoryCarousel {\n  linkText\n  titleCarousel\n  applink\n  list {\n    categoryID\n    icon\n    title\n    isApplink\n    applink\n    __typename\n  }\n  __typename\n}\n\nfragment ProductHighlight on pdpDataProductContent {\n  name\n  price {\n    value\n    currency\n    __typename\n  }\n  campaign {\n    campaignID\n    campaignType\n    campaignTypeName\n    campaignIdentifier\n    background\n    percentageAmount\n    originalPrice\n    discountedPrice\n    originalStock\n    stock\n    stockSoldPercentage\n    threshold\n    startDate\n    endDate\n    endDateUnix\n    appLinks\n    isAppsOnly\n    isActive\n    hideGimmick\n    __typename\n  }\n  thematicCampaign {\n    additionalInfo\n    background\n    campaignName\n    icon\n    __typename\n  }\n  stock {\n    useStock\n    value\n    stockWording\n    __typename\n  }\n  variant {\n    isVariant\n    parentID\n    __typename\n  }\n  wholesale {\n    minQty\n    price {\n      value\n      currency\n      __typename\n    }\n    __typename\n  }\n  isCashback {\n    percentage\n    __typename\n  }\n  isTradeIn\n  isOS\n  isPowerMerchant\n  isWishlist\n  isCOD\n  preorder {\n    duration\n    timeUnit\n    isActive\n    preorderInDays\n    __typename\n  }\n  __typename\n}\n\nfragment ProductCustomInfo on pdpDataCustomInfo {\n  icon\n  title\n  isApplink\n  applink\n  separator\n  description\n  __typename\n}\n\nfragment ProductInfo on pdpDataProductInfo {\n  row\n  content {\n    title\n    subtitle\n    applink\n    __typename\n  }\n  __typename\n}\n\nfragment ProductDetail on pdpDataProductDetail {\n  content {\n    title\n    subtitle\n    applink\n    showAtFront\n    isAnnotation\n    __typename\n  }\n  __typename\n}\n\nfragment ProductDataInfo on pdpDataInfo {\n  icon\n  title\n  isApplink\n  applink\n  content {\n    icon\n    text\n    __typename\n  }\n  __typename\n}\n\nfragment ProductSocial on pdpDataSocialProof {\n  row\n  content {\n    icon\n    title\n    subtitle\n    applink\n    type\n    rating\n    __typename\n  }\n  __typename\n}\n\nquery PDPGetLayoutQuery($shopDomain: String, $productKey: String, $layoutID: String, $apiVersion: Float, $userLocation: pdpUserLocation, $extParam: String, $tokonow: pdpTokoNow) {\n  pdpGetLayout(shopDomain: $shopDomain, productKey: $productKey, layoutID: $layoutID, apiVersion: $apiVersion, userLocation: $userLocation, extParam: $extParam, tokonow: $tokonow) {\n    requestID\n    name\n    pdpSession\n    basicInfo {\n      alias\n      createdAt\n      isQA\n      id: productID\n      shopID\n      shopName\n      minOrder\n      maxOrder\n      weight\n      weightUnit\n      condition\n      status\n      url\n      needPrescription\n      catalogID\n      isLeasing\n      isBlacklisted\n      isTokoNow\n      menu {\n        id\n        name\n        url\n        __typename\n      }\n      category {\n        id\n        name\n        title\n        breadcrumbURL\n        isAdult\n        isKyc\n        minAge\n        detail {\n          id\n          name\n          breadcrumbURL\n          isAdult\n          __typename\n        }\n        __typename\n      }\n      txStats {\n        transactionSuccess\n        transactionReject\n        countSold\n        paymentVerified\n        itemSoldFmt\n        __typename\n      }\n      stats {\n        countView\n        countReview\n        countTalk\n        rating\n        __typename\n      }\n      __typename\n    }\n    components {\n      data {\n        ...ProductMedia\n        ...ProductHighlight\n        ...ProductInfo\n        ...ProductDetail\n        ...ProductSocial\n        ...ProductDataInfo\n        ...ProductCustomInfo\n        ...ProductVariant\n        ...ProductCategoryCarousel\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n"
-    }
-  ]);
-
-  let config = {
-    method: 'post',
-    maxBodyLength: Infinity,
-    url: 'https://gql.tokopedia.com/graphql/PDPGetLayoutQuery',
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0',
-      'Accept': '*/*',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Origin': 'https://www.tokopedia.com',
-      'Connection': 'keep-alive',
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'no-cors',
-      'Sec-Fetch-Site': 'same-site',
-      'TE': 'trailers',
-      'X-Tkpd-Lite-Service': 'zeus',
-      'X-Version': 'a61214f',
-      'content-type': 'application/json',
-      'x-device': 'desktop',
-      'X-TKPD-AKAMAI': 'pdpGetLayout',
-      'X-Source': 'tokopedia-lite',
-      'Pragma': 'no-cache',
-      'Cache-Control': 'no-cache'
-    },
-    data: data
-  };
-
-  const res = await axios.request(config)
-  console.log(res.data)
-  return res.data
-}
-
-main()
+await main()
