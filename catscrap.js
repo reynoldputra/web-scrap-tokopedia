83a6b76c506cb2d90845c7f2a884e1874f3b2a2e
reynoldputra <reynoldputra1@gmail.com>
2023-07-02 15:03:01 +0700

    fix: retry request is error

--- catscrap.js
+++ catscrap.js
@@ -2,76 +2,41 @@ import csv from "csvtojson"
 import { getCatUrl, getDataFromHtml, getShopInfoFromHtml, scrapCatList } from "./lib/catscraplib.js"
 import { productScrap } from "./lib/scrap.js"
 import { writeToJson } from "./lib/write.js"
+import pMap from "p-map"
 
 const main = async () => {
   const json = await csv()
     .fromFile('./data/catmapping.csv')
 
-  const concurrencyLimit = 1
+  const catlist = await scrapCatList()
 
-  for (let i = 0; i < json.length; i += concurrencyLimit) {
-    const batchJson = json.slice(i, i + concurrencyLimit);
-    const promises = batchJson.map(catObj => scrapCategories(catObj));
-    await Promise.all(promises);
+  for(const idx in json) {
+    await scrapCategories(json[idx], catlist)
   }
 
 }
 
-const scrapCategories = (catObj) => {
+const scrapCategories = (catObj, catlist) => {
   const cat = [catObj.cat1_id, catObj.cat2_id, catObj.cat3_id]
+  const catSlug = cat.join("-")
   return new Promise(async (resolve) => {
-    const catlist = await scrapCatList()
     const catUrl = await getCatUrl(catlist, cat)
-
     let i = 1;
     while (true) {
       try {
-
         const allCardsProduct = await getDataFromHtml(catUrl + `?page=${i}`)
         if (allCardsProduct.length == 0) break
-        i++;
-        const scrapResult = []
-        for (const idx in allCardsProduct) {
-          const product = allCardsProduct[idx]
-          const gaKey = product.ga_key;
-          const routes = gaKey.split("/");
-          let productSlug = routes[routes.length - 1];
-          let shopDomain = routes[routes.length - 2];
-          const pdRes = await productScrap(productSlug, shopDomain);
-          if (!pdRes) {
-            console.log("Skipped")
-            continue
-          }
 
-          const productInfo = pdRes.basicInfo;
-          let catSlug = `${productInfo.category.detail[0].name}-${productInfo.category.detail[1].name}-${productInfo.category.detail[2].name}`;
-
-          let shopUrl = product.url.split("/").slice(0, 4).join("/")
-
-          let shopInfoAdd = await getShopInfoFromHtml(shopUrl)
-
-          const newData = {
-            cat_slug: catSlug,
-            itemid: product.id.toString(),
-            shopid: productInfo.shopID,
-            product_title: product.name,
-            product_link: product.url,
-            brand: null,
-            store_type: shopInfoAdd.isOfficial,
-            store_name: productInfo.shopName,
-            store_link: shopUrl,
-            store_location: shopInfoAdd.location,
-            price: product.price_int,
-            rating: productInfo.stats.rating,
-            historical_sold: parseInt(productInfo.txStats.countSold),
-            review_count: parseInt(productInfo.stats.countReview),
-            view_count: parseInt(productInfo.stats.countView)
-          };
-
-          console.log(newData)
-          scrapResult.push(newData)
-          writeToJson("./export/cat.json", newData)
+        const allresult = []
+        const mapper = async product => {
+          const result = await scrapCategory(product)
+          if (result)
+            allresult.push(result)
         }
+        await pMap(allCardsProduct, mapper, { concurrency: allCardsProduct.length })
+        writeToJson(`./export/bycat/${catSlug}.json`, allresult)
+        console.log(`${catUrl} page ${i} done`)
+        i++;
       } catch (err) {
         console.log("Error : ", err)
       }
@@ -79,6 +44,53 @@ const scrapCategories = (catObj) => {
     resolve(scrapResult)
   })
 
+}
+
+const scrapCategory = async (product) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const gaKey = product.ga_key;
+      const routes = gaKey.split("/");
+      let productSlug = routes[routes.length - 1];
+      let shopDomain = routes[routes.length - 2];
+      const pdRes = await productScrap(productSlug, shopDomain);
+      if (!pdRes) {
+        console.log("Skipped")
+        reject([])
+      }
+
+      const productInfo = pdRes.basicInfo;
+      let catSlug = `${productInfo.category.detail[0].name}-${productInfo.category.detail[1].name}-${productInfo.category.detail[2].name}`;
+
+      let shopUrl = product.url.split("/").slice(0, 4).join("/")
+
+      let shopInfoAdd = await getShopInfoFromHtml(shopUrl)
+
+      const newData = {
+        cat_slug: catSlug,
+        itemid: product.id.toString(),
+        shopid: productInfo.shopID,
+        product_title: product.name,
+        product_link: product.url,
+        brand: null,
+        store_type: shopInfoAdd.isOfficial,
+        store_name: productInfo.shopName,
+        store_link: shopUrl,
+        store_location: shopInfoAdd.location,
+        price: product.price_int,
+        rating: productInfo.stats.rating,
+        historical_sold: parseInt(productInfo.txStats.countSold),
+        review_count: parseInt(productInfo.stats.countReview),
+        view_count: parseInt(productInfo.stats.countView)
+      };
+
+      console.log(product.name)
+      resolve(newData)
+    } catch (err) {
+      console.log("Error: ", err)
+      reject([])
+    }
+  })
 }
 
 await main()
--- keywordscrap.js
+++ keywordscrap.js
@@ -1,76 +1,89 @@
-import { keywordScrapAllPage, productScrap } from "./lib/scrap.js";
+import { keywordScrap, productScrap } from "./lib/scrap.js";
 import { toInt } from "./lib/utils.js";
 import csv from "csvtojson"
 import { writeToJson } from "./lib/write.js";
+import pMap from "p-map";
 
 const main = async () => {
   const json = await csv()
     .fromFile('./data/keywordtoscrap.csv')
   const keywords = json.map((obj) => obj.keyword)
-  const concurrencyLimit = 1
 
-  for (let i = 0; i < keywords.length; i += concurrencyLimit) {
-    const batchKeywords = keywords.slice(i, i + concurrencyLimit);
-    const promises = batchKeywords.map(keyword => scrapProductbyKeyword(keyword));
-    await Promise.all(promises);
+  for (const idx in keywords) {
+    await scrapProductbyKeyword(keywords[idx])
   }
 
 }
 
 const scrapProductbyKeyword = async (keyword) => {
   return new Promise(async (resolve) => {
-    const products = await keywordScrapAllPage(keyword)
-    const scrapResult = []
+    let i = 1
+    while (true) {
+      const kwRes = await keywordScrap(keyword, i)
+      const kwScrapData = kwRes.data["ace_search_product_v4"]
+      if (kwScrapData.header.totalData == 0) {
+        resolve(products)
+      }
 
-    for (const idx in products) {
+      const products = kwScrapData.data.products
+
+      const allresult = []
       try {
-        const product = products[idx]
-        console.log(product)
-        const gaKey = product.gaKey;
-        const routes = gaKey.split("/");
-        let productSlug = routes[routes.length - 1];
-        let shopDomain = routes[routes.length - 2];
-        const shopId = product.shop.shopId.toString();
-        const pdRes = await productScrap(productSlug, shopDomain);
-        if (!pdRes) {
-          console.log("Skipped")
-          continue
+        const mapper = async product => {
+          const result = await scrapProduct(product, keyword)
+          allresult.push(result)
         }
-        const productInfo = pdRes.basicInfo;
-
-        let catSlug = `${productInfo.category.detail[0].name}-${productInfo.category.detail[1].name}-${productInfo.category.detail[2].name}`;
-
-        const newData = {
-          keyword,
-          cat_slug: catSlug,
-          itemid: product.id.toString(),
-          shopid: shopId,
-          product_title: product.name,
-          product_link: product.url,
-          brand: null,
-          store_type: product.shop.isOfficial,
-          store_name: product.shop.name,
-          store_link: product.shop.url,
-          store_location: product.shop.city,
-          price: toInt(product.price),
-          rating: productInfo.stats.rating,
-          historical_sold: parseInt(productInfo.txStats.countSold),
-          review_count: parseInt(productInfo.stats.countReview),
-          view_count: parseInt(productInfo.stats.countView)
-        };
-
-        console.log(newData);
-        writeToJson(`./export/result.json`, newData)
-        scrapResult.push(newData);
+        await pMap(products, mapper, { concurrency: 50 })
+        writeToJson(`./export/bykeyword/${keyword}.json`, allresult)
       } catch (err) {
         console.log("Error : ", err)
       }
-    };
-
-    // writeToJson(`./export/result.json`, scrapResult)
-    resolve(scrapResult)
+      i++;
+    }
   })
 
+}
+
+const scrapProduct = (product, keyword) => {
+  return new Promise(async (resolve, reject) => {
+    const gaKey = product.gaKey;
+    const routes = gaKey.split("/");
+    let productSlug = routes[routes.length - 1];
+    let shopDomain = routes[routes.length - 2];
+    const shopId = product.shop.shopId.toString();
+    const pdRes = await productScrap(productSlug, shopDomain);
+    if (!pdRes) {
+      console.log(pdRes)
+      console.log("Skipped")
+      reject([])
+      return
+    }
+    const productInfo = pdRes.basicInfo;
+
+    let catSlug = `${productInfo.category.detail[0].name}-${productInfo.category.detail[1].name}-${productInfo.category.detail[2].name}`;
+
+    const newData = {
+      keyword,
+      cat_slug: catSlug,
+      itemid: product.id.toString(),
+      shopid: shopId,
+      product_title: product.name,
+      product_link: product.url,
+      brand: null,
+      store_type: product.shop.isOfficial,
+      store_name: product.shop.name,
+      store_link: product.shop.url,
+      store_location: product.shop.city,
+      price: toInt(product.price),
+      rating: productInfo.stats.rating,
+      historical_sold: parseInt(productInfo.txStats.countSold),
+      review_count: parseInt(productInfo.stats.countReview),
+      view_count: parseInt(productInfo.stats.countView)
+    };
+
+    console.log(product.name);
+    resolve(newData)
+  })
 }
 
 await main()
