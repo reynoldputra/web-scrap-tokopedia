36c367c28d6e0da011fc473b92fc637df18ce1ba
reynoldputra <reynoldputra1@gmail.com>
2023-07-03 23:27:44 +0700

    feat: refactor writing file

--- catscrap.js
+++ catscrap.js
@@ -1,64 +1,100 @@
-import csv from "csvtojson"
-import { getCatUrl, getDataFromHtml, scrapCatList } from "./lib/catscraplib.js"
-import { productScrap } from "./lib/scrap.js"
-import { writeToJson } from "./lib/write.js"
-import pMap from "p-map"
-import * as fs from "fs"
-import jsonrawtoxlsx from "jsonrawtoxlsx"
+import csv from "csvtojson";
+import { getCatUrl, getDataFromHtml, scrapCatList } from "./lib/catscraplib.js";
+import { productScrap } from "./lib/scrap.js";
+// import { writeToJson } from "./lib/write.js";
+import pMap from "p-map";
+import * as fs from "fs";
+// import jsonrawtoxlsx from "jsonrawtoxlsx";
+import { byCategory } from "./task.js";
+import { writeJsonFileSync } from "write-json-file";
 
 const main = async () => {
-  const json = await csv()
-    .fromFile('./data/catmapping.csv')
+  const arg = process.argv[2];
+  const json = await csv().fromFile("./data/catmapping.csv");
+  const catlist = await scrapCatList();
 
-  const catlist = await scrapCatList()
+  // If an argument is provided, only run for that category
+  const categories = arg ? { [arg]: byCategory[arg] } : byCategory;
 
-  for (const idx in json) {
-    await scrapCategories(json[idx], catlist)
+  // Loop over each category in byCategory object or a specific category
+  for (let item in categories) {
+    let itemName = categories[item].name;
+    if (!fs.existsSync(`./export/bycat/${itemName}`)) fs.mkdirSync(`./export/bycat/${itemName}`);
+    // filter the JSON entries (from catmapping.csv) based on the current category
+    let filteredJson = json.filter((row) => {
+      return categories[item].lis_category
+        .map((category) => category.toLowerCase())
+        .includes(row[categories[item].cat].toLowerCase());
+    });
+
+    const mapper = async (idx) => {
+      await scrapCategories({ ...filteredJson[idx], name: categories[item].name }, catlist);
+    };
+
+    // Use pMap to handle concurrency
+    await pMap(Object.keys(filteredJson), mapper, { concurrency: 3 });
+
+    if (!fs.existsSync(`${categories[item].name}`)) fs.mkdirSync(`${categories[item].name}`);
+
+    // const date = new Date();
+    // const fileName = `./export/byCat/${
+    //   categories[item].name
+    // }/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byCat.json`;
+    // const fileNameX = `./export/byCat/${
+    //   categories[item].name
+    // }/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byCat.xlsx`;
+    // console.log("write excel");
+    // fs.readFile(fileName, "utf8", (err, data) => {
+    //   if (err) throw err;
+    //   const json = JSON.parse(data);
+    //   const bufferExcel = jsonrawtoxlsx(json);
+    //   fs.writeFileSync(fileNameX, bufferExcel, "binary");
+    // });
   }
-
-  const date = new Date()
-  const fileName = `./export/bykeyword/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byKeyword.json`
-  const fileNameX = `./export/bykeyword/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byKeyword.xlsx`
-  console.log("write excel")
-  fs.readFile(fileName, 'utf8', (err, data) => {
-    const json = JSON.parse(data)
-    const bufferExcel = jsonrawtoxlsx(json)
-    fs.writeFileSync(fileNameX, bufferExcel, 'binary')
-  })
-}
+};
 
 const scrapCategories = (catObj, catlist) => {
-  const cat = [catObj.cat1_id, catObj.cat2_id, catObj.cat3_id]
-  const date = new Date()
-  const fileName = `./export/bycat/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byCat.json`
+  const cat = [catObj.cat1_id, catObj.cat2_id, catObj.cat3_id];
+  const catSlug = [catObj.cat1, catObj.cat2, catObj.cat3].join("-");
+  const date = new Date();
+
+  const catFolder = `./export/bycat/${catObj.name}/${catSlug}`;
+  if (!fs.existsSync(catFolder)) fs.mkdirSync(catFolder);
+
   return new Promise(async (resolve) => {
-    const catUrl = await getCatUrl(catlist, cat)
+    const catUrl = await getCatUrl(catlist, cat);
     let i = 1;
     while (true) {
+      const allresult = [];
       try {
-        const allCardsProduct = await getDataFromHtml(catUrl + `?page=${i}`)
-        if (allCardsProduct.length == 0) break
+        const allCardsProduct = await getDataFromHtml(catUrl + `?page=${i}`);
+        console.log(`${catSlug} page ${i} ${allCardsProduct.length} products`);
+        if (allCardsProduct.length == 0) break;
 
-        const allresult = []
-        const mapper = async product => {
-          const result = await scrapCategory(product)
-          if (result)
-            allresult.push(result)
-        }
-        await pMap(allCardsProduct, mapper, { concurrency: allCardsProduct.length })
-        writeToJson(fileName, allresult)
-        console.log(`${catUrl} page ${i} done`)
+        const mapper = async (product) => {
+          const result = await scrapCategory(product, i);
+          if (result) allresult.push(result);
+        };
+        await pMap(allCardsProduct, mapper, {
+          concurrency: allCardsProduct.length
+        });
+        //clear console and print progress
+        // process.stdout.write("\x1B[2J\x1B[0f");
+        console.log(`${catSlug} page ${i} done` + " ");
+        const fileNameSuccess = `${catFolder}/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byKeyword_page${i}_success.json`;
+        writeJsonFileSync(fileNameSuccess, allresult);
         i++;
       } catch (err) {
-        console.log("Error : ", err)
+        const fileNameError = `${catFolder}/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byKeyword_page${i}_error.json`;
+        writeJsonFileSync(fileNameError, allresult);
+        console.log(`Error on ${catSlug} page ${i}`);
       }
     }
-    resolve([])
-  })
+    resolve([]);
+  });
+};
 
-}
-
-const scrapCategory = async (product) => {
+const scrapCategory = async (product, page) => {
   return new Promise(async (resolve, reject) => {
     try {
       const gaKey = product.ga_key;
@@ -66,13 +102,13 @@ const scrapCategory = async (product) => {
       let productSlug = routes[routes.length - 1];
       let shopDomain = routes[routes.length - 2];
       const newData = await productScrap(productSlug, shopDomain);
-      console.log(newData);
-      resolve(newData)
+      // console.log(newData);
+      resolve(newData);
     } catch (err) {
-      console.log("Error: ", err)
-      reject([])
+      console.log("Error on page ", page, ":", err);
+      reject([]);
     }
-  })
-}
+  });
+};
 
-await main()
+await main();
--- keywordscrap.js
+++ keywordscrap.js
@@ -1,64 +1,79 @@
 import { keywordScrap, productScrap } from "./lib/scrap.js";
-import csv from "csvtojson"
+// import csv from "csvtojson";
 import pMap from "p-map";
-import { writeToJson } from "./lib/write.js";
-import * as fs from "fs"
-import jsonrawtoxlsx from "jsonrawtoxlsx"
+import * as fs from "fs";
+// import { writeToJson } from "./lib/write.js";
+// import jsonrawtoxlsx from "jsonrawtoxlsx";
+import { byKeyword } from "./task.js";
+import { writeJsonFileSync } from "write-json-file";
 
 const main = async () => {
-  const json = await csv()
-    .fromFile('./data/keywordtoscrap.csv')
-  const keywords = json.map((obj) => obj.keyword)
+  const arg = process.argv[2];
 
-  for (const idx in keywords) {
-    await scrapProductbyKeyword(keywords[idx])
-  }
-  const date = new Date()
+  // If an argument is provided, only run for that keyword
+  const tasks = arg ? { [arg]: byKeyword[arg] } : byKeyword;
 
-  const fileName = `./export/bykeyword/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byKeyword.json`
-  const fileNameX = `./export/bykeyword/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byKeyword.xlsx`
-  console.log("write excel")
-  fs.readFile(fileName, 'utf8', (err, data) => {
-    const json = JSON.parse(data)
-    const bufferExcel = jsonrawtoxlsx(json)
-    fs.writeFileSync(fileNameX, bufferExcel, 'binary')
-  })
-}
-
-const scrapProductbyKeyword = async (keyword) => {
-  const date = new Date()
-  const fileName = `./export/bykeyword/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byKeyword.json`
-  return new Promise(async (resolve) => {
-    let i = 1
-    while (true) {
-      try {
-        const kwRes = await keywordScrap(keyword, i)
-        const kwScrapData = kwRes.data["ace_search_product_v4"]
-        if (kwScrapData.header.totalData == 0) {
-
-          resolve([])
-        }
-
-        const products = kwScrapData.data.products
-
-        const allresult = []
-        const mapper = async product => {
-          const result = await scrapProduct(product, keyword, i)
-          allresult.push(result)
-        }
-        await pMap(products, mapper, { concurrency: 100 })
-        writeToJson(fileName, allresult)
-
-
-
-      } catch (err) {
-        console.log("Error : ", err)
-      }
-      i++;
+  // Loop over each item in the tasks object
+  for (let item in tasks) {
+    if (!fs.existsSync(`./export/bykeyword/${tasks[item].name}`))
+      fs.mkdirSync(`./export/bykeyword/${tasks[item].name}`);
+    // Loop over each keyword in the current item's list_keywords array
+    for (let keyword of tasks[item].list_keywords) {
+      if (!fs.existsSync(`./export/bykeyword/${tasks[item].name}/${keyword}`))
+        fs.mkdirSync(`./export/bykeyword/${tasks[item].name}/${keyword}`);
+      await scrapProductbyKeyword(keyword, tasks[item].name); // Note the second argument here
     }
-  })
 
-}
+    // const date = new Date();
+    // const fileName = `./export/bykeyword/${
+    //   tasks[item].name
+    // }/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byKeyword.json`;
+    // const fileNameX = `./export/bykeyword/${
+    //   tasks[item].name
+    // }/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byKeyword.xlsx`;
+    // console.log("write excel");
+    // fs.readFile(fileName, "utf8", (err, data) => {
+    //   if (err) throw err;
+    //   const json = JSON.parse(data);
+    //   const bufferExcel = jsonrawtoxlsx(json);
+    //   fs.writeFileSync(fileNameX, bufferExcel, "binary");
+    // });
+  }
+};
+
+const scrapProductbyKeyword = async (keyword, taskName) => {
+  const date = new Date();
+  return new Promise(async (resolve) => {
+    let i = 1;
+    while (true) {
+      const allresult = [];
+      try {
+        const kwRes = await keywordScrap(keyword, i);
+        const kwScrapData = kwRes.data["ace_search_product_v4"];
+        if (kwScrapData.header.totalData == 0) {
+          resolve([]);
+          break;
+        }
+
+        const products = kwScrapData.data.products;
+        const mapper = async (product) => {
+          const result = await scrapProduct(product, keyword, i);
+          allresult.push(result);
+        };
+        await pMap(products, mapper, { concurrency: 100 });
+        const fileNameSuccess = `./export/bykeyword/${taskName}/${keyword}/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byKeyword_page${i}_success.json`;
+        writeJsonFileSync(fileNameSuccess, allresult);
+        console.log(`Page ${i} keyword ${keyword} done`);
+        i++;
+      } catch (err) {
+        const fileNameError = `./export/bykeyword/${taskName}/${keyword}/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}byKeyword_page${i}_error.json`;
+        writeJsonFileSync(fileNameSuccess, fileNameError);
+        console.log("Error : ", err);
+      }
+    }
+    resolve([]);
+  });
+};
 
 const scrapProduct = (product, keyword, page) => {
   return new Promise(async (resolve) => {
@@ -67,9 +82,9 @@ const scrapProduct = (product, keyword, page) => {
     let productSlug = routes[routes.length - 1];
     let shopDomain = routes[routes.length - 2];
     const newData = await productScrap(productSlug, shopDomain, keyword);
-    console.log(page, newData);
-    resolve(newData)
-  })
-}
+    console.log(page, newData.product_title);
+    resolve(newData);
+  });
+};
 
-await main()
+await main();
